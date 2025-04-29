c50cfc4d689f2eeeecb937cb183ff533dc38c8e7
sheymor21 <joseacvz81@gmail.com>
2025-04-29 12:52:35 -0400

    Add new database test cases and improve error messages
    Introduced tests for additional functionality like row/column updates, deletions, and value additions. Enhanced error messages to provide more context with received values for easier debugging.

--- Test/suite_test.go
+++ Test/suite_test.go
@@ -1,8 +1,10 @@
 package Test
 
 import (
+	"fmt"
 	"github.com/stretchr/testify/suite"
 	"os"
+	"strings"
 	"testing"
 	"text-database/pkg"
 	"text-database/pkg/utilities"
@@ -23,7 +25,7 @@ func (s *Suite) TearDownTest() {
 func (s *Suite) TestGetTableByName() {
 	tb := s.db.GetTableByName("Users")
 	if tb.GetName() != "-----Users-----" {
-		s.Fail("Expected Users Table")
+		s.Fail("Expected Users Table", fmt.Sprintf("Recibe: %s", tb))
 
 	}
 }
@@ -34,14 +36,29 @@ func (s *Suite) TestGetTables() {
 	}
 	for _, t := range tbs {
 		if t.GetName() != "-----Users-----" {
-			s.Fail("Expected Users Table")
+			s.Fail("Expected Users Table", fmt.Sprintf("Recibe: %s", t))
 		}
+	}
+}
+func (s *Suite) TestGetRowById() {
+	tb := s.db.GetTableByName("Users")
+	row := tb.GetRowById("2")
+	row = strings.TrimSpace(row)
+	if row != "|1| 2 |2| juan |3| 54" {
+		s.Fail("Expected |1| 2 |2| juan |3| 54", fmt.Sprintf("Recibe: %s", row))
+	}
+}
+func (s *Suite) TestGetRows() {
+	tb := s.db.GetTableByName("Users")
+	count := len(tb.GetRows())
+	if count != 2 {
+		s.Fail("Expected 2 Rows", fmt.Sprintf("Recibe: %d", count))
 	}
 }
 func (s *Suite) TestNewTable() {
 	tb := s.db.NewTable("Test", []string{"name", "age"})
 	if tb.GetName() != "-----Test-----" {
-		s.Fail("Expected Test Table")
+		s.Fail("Expected Test Table", fmt.Sprintf("Recibe: %s", tb))
 	}
 }
 func (s *Suite) TestDeleteTable() {
@@ -50,11 +67,72 @@ func (s *Suite) TestDeleteTable() {
 	tbs := s.db.GetTables()
 	for _, t := range tbs {
 		if t.GetName() == "-----Test-----" {
-			s.Fail("Expected Test Table deleted")
+			s.Fail("Expected Test Table deleted", fmt.Sprintf("Recibe: %s", t))
 		}
 	}
 }
+func (s *Suite) TestAddValue() {
+	tb := s.db.GetTableByName("Users")
+	tb = tb.AddValue("name", "Jose")
+	rows := tb.GetRows()
+	id := getId(rows[2])
+	if rows[2] != fmt.Sprintf("|1| %s |2| Jose |3| null ", id) {
+		s.Fail(fmt.Sprintf("Expected |1| %s |2| Jose |3| null", id), fmt.Sprintf("Recibe: %s", rows[2]))
+	}
 
-func TestAll(t *testing.T) {
+}
+func (s *Suite) TestAddValues() {
+	tb := s.db.GetTableByName("Users")
+	tb = tb.AddValues([]string{"Jose", "20"})
+	rows := tb.GetRows()
+	id := getId(rows[2])
+	if rows[2] != fmt.Sprintf("|1| %s |2| Jose |3| 20 ", id) {
+		s.Fail(fmt.Sprintf("Expected |1| %s |2| Jose |3| 20", id), fmt.Sprintf("Recibe: %s", rows[2]))
+	}
+}
+func (s *Suite) TestUpdateTableName() {
+	tb := s.db.GetTableByName("Users")
+	tb = tb.UpdateTableName("Test")
+	if tb.GetName() != "-----Test-----" {
+		s.Fail("Expected Test Table", fmt.Sprintf("Recibe: %s", tb))
+	}
+}
+func (s *Suite) TestUpdateColumnName() {
+	tb := s.db.GetTableByName("Users")
+	tb = tb.UpdateColumnName("name", "username")
+	columns := strings.TrimSpace(strings.Join(tb.GetColumns(), " "))
+	if columns != "[1] id [2] username [3] age" {
+		s.Fail("Expected [1] id [2] username |3| age ", fmt.Sprintf("Recibe: %s", columns))
+	}
+}
+func (s *Suite) TestUpdateValue() {
+	tb := s.db.GetTableByName("Users")
+	tb = tb.UpdateValue("age", "2", "30")
+	rows := tb.GetRows()
+	if rows[1] != "|1| 2 |2| juan |3| 30 " {
+		s.Fail("Expected |1| 2 |2| juan |3| 30", fmt.Sprintf("Recibe: %s", rows[1]))
+	}
+}
+func (s *Suite) TestDeleteRow() {
+	tb := s.db.GetTableByName("Users")
+	tb = tb.DeleteRow("1")
+	rows := tb.GetRows()
+	if rows[0] != "|1| 2 |2| juan |3| 54" || len(rows) != 1 {
+		s.Fail("Expected |1| 1 |2| juan |3| 54", fmt.Sprintf("Recibe: %s", rows[0]))
+	}
+}
+func (s *Suite) TestDeleteColumn() {
+	tb := s.db.GetTableByName("Users")
+	tb = tb.DeleteColumn("age")
+	columns := strings.TrimSpace(strings.Join(tb.GetColumns(), " "))
+	if columns != "[1] id [2] name" {
+		s.Fail("Expected [1] id [2] name", fmt.Sprintf("Recibe: %s", columns))
+	}
+}
+func TestDatabase(t *testing.T) {
 	suite.Run(t, new(Suite))
+}
+func getId(row string) string {
+	split := strings.Split(row, "|")
+	return strings.TrimSpace(split[2])
 }
