d4dae8a87846d18b357d5b2c296d8bd32acbf20e
sheymor21 <joseacvz81@gmail.com>
2025-06-09 18:23:37 -0400

    Add flag for custom migration name and enhance database initialization
    Integrated flag parsing to allow users to specify a custom migration name via the `-ma` flag. Enhanced `CreateDatabase` with new `DataConfig` for table and value initialization, improving database setup flexibility. Included error handling for database creation.

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,11 +1,35 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"text-database/pkg"
 )
 
 func main() {
-	config := pkg.DbConfig{SecurityKey: "", DatabaseName: "database.txt"}
-	config.CreateDatabase()
+	data := []pkg.DataConfig{
+		{
+			TableName: "Esto",
+			Columns:   []string{"name", "age"},
+			Values:    []pkg.Values{{"carlos", "32"}, {"jose", "23"}},
+		},
+
+		{
+			TableName: "Segunda Tabla",
+			Columns:   []string{"Nombre", "Direccion"},
+			Values:    []pkg.Values{{"Perez", "Manuguayabo"}},
+		},
+	}
+	config := pkg.DbConfig{EncryptionKey: "", DatabaseName: "database.txt", DataConfig: data}
+	_, err := config.CreateDatabase()
+	migrationName := flag.String("ma", "", "Name of the migration")
+	flag.Parse()
+	if migrationName != nil {
+		config.CreateMigration(*migrationName)
+	}
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 
 }
